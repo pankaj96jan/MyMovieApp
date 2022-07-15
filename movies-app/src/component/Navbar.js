84a4e472f8c0e750eea7c1c9002e41601017ff6d
soniPankaj1996 <sonipankaj1996@gmail.com>
2022-07-15 14:38:06 +0530

    this is app bar with menu items

--- movies-app/src/component/Navbar.js
+++ movies-app/src/component/Navbar.js
@@ -1,57 +1,53 @@
-import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import Badge from '@mui/material/Badge';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import MailIcon from '@mui/icons-material/Mail';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import MoreIcon from '@mui/icons-material/MoreVert';
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import MoreIcon from "@mui/icons-material/MoreVert";
 
-const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
+const Search = styled("div")(({ theme }) => ({
+    position: "relative",
     borderRadius: theme.shape.borderRadius,
     backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
+    "&:hover": {
         backgroundColor: alpha(theme.palette.common.white, 0.25),
     },
     marginRight: theme.spacing(2),
     marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
         marginLeft: theme.spacing(3),
-        width: 'auto',
+        width: "auto",
     },
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
+const SearchIconWrapper = styled("div")(({ theme }) => ({
     padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
+    color: "inherit",
+    "& .MuiInputBase-input": {
         padding: theme.spacing(1, 1, 1, 0),
         // vertical padding + font size from searchIcon
         paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('md')]: {
-            width: '20ch',
+        transition: theme.transitions.create("width"),
+        width: "100%",
+        [theme.breakpoints.up("md")]: {
+            width: "20ch",
         },
     },
 }));
@@ -80,41 +76,41 @@ export default function Navbar() {
         setMobileMoreAnchorEl(event.currentTarget);
     };
 
-    const menuId = 'primary-search-account-menu';
+    const menuId = "primary-search-account-menu";
     const renderMenu = (
         <Menu
             anchorEl={anchorEl}
             anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
             }}
             id={menuId}
             keepMounted
             transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
             }}
             open={isMenuOpen}
             onClose={handleMenuClose}
         >
             <MenuItem onClick={handleMenuClose}>Login</MenuItem>
-            <MenuItem onClick={handleMenuClose}>My Movies</MenuItem>
+            <MenuItem onClick={handleMenuClose}>signup</MenuItem>
         </Menu>
     );
 
-    const mobileMenuId = 'primary-search-account-menu-mobile';
+    const mobileMenuId = "primary-search-account-menu-mobile";
     const renderMobileMenu = (
         <Menu
             anchorEl={mobileMoreAnchorEl}
             anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
             }}
             id={mobileMenuId}
             keepMounted
             transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
             }}
             open={isMobileMenuOpen}
             onClose={handleMobileMenuClose}
@@ -156,7 +152,13 @@ export default function Navbar() {
 
     return (
         <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static" sx={{ background: "linear-gradient(90deg, rgba(121,9,53,1) 0%, rgba(13,228,196,1) 56%, rgba(121,9,53,1) 100%)" }} >
+            <AppBar
+                position="static"
+                sx={{
+                    background:
+                        "linear-gradient(90deg, rgba(121,9,53,1) 0%, rgba(13,228,196,1) 56%, rgba(121,9,53,1) 100%)",
+                }}
+            >
                 <Toolbar>
                     {/* <IconButton
                         size="large"
@@ -172,23 +174,21 @@ export default function Navbar() {
                         variant="h5"
                         noWrap
                         component="div"
-                        sx={{ display: { xs: 'none', sm: 'block' } }}
+                        sx={{ display: { xs: "none", sm: "block" } }}
                     >
                         MOVIES-APP
                     </Typography>
-                    <Search 
-                    sx={{ flexGrow: 2 }}
-                    >
+                    <Search sx={{ flexGrow: 1.5 }}>
                         <SearchIconWrapper>
                             <SearchIcon />
                         </SearchIconWrapper>
                         <StyledInputBase
                             placeholder="Search by Name..."
-                            inputProps={{ 'aria-label': 'search' }}
+                            inputProps={{ "aria-label": "search" }}
                         />
                     </Search>
                     <Box sx={{ flexGrow: 1 }} />
-                    <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                    <Box sx={{ display: { xs: "none", md: "flex" } }}>
                         {/* <IconButton size="large" aria-label="show 4 new mails" color="inherit">
                             <Badge badgeContent={4} color="error">
                                 <MailIcon />
@@ -203,6 +203,23 @@ export default function Navbar() {
                                 <NotificationsIcon />
                             </Badge>
                         </IconButton> */}
+                         <IconButton color="inherit" sx={{ mx: "1rem" }}>
+                            <Typography sx={{ fontFamily: "Roboto" }} variant="h6">
+                                ADD MOVIE+
+                            </Typography>
+                        </IconButton>
+                        <IconButton color="inherit" sx={{ mx: "1rem" }}>
+                            <Typography variant="h6" sx={{ fontFamily: "Roboto" }}>
+                                My Wishlist
+                            </Typography>
+                        </IconButton>
+                        <IconButton color="inherit" sx={{ mx: "1rem" }}>
+                            <Typography sx={{ fontFamily: "Roboto" }} variant="h6">
+                                This Weekend
+                            </Typography>
+                        </IconButton>
+                        
+
                         <IconButton
                             size="large"
                             edge="end"
@@ -215,7 +232,7 @@ export default function Navbar() {
                             <AccountCircle />
                         </IconButton>
                     </Box>
-                    <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+                    <Box sx={{ display: { xs: "flex", md: "none" } }}>
                         <IconButton
                             size="large"
                             aria-label="show more"
@@ -234,3 +251,5 @@ export default function Navbar() {
         </Box>
     );
 }
+
+// import ExitToAppIcon from '@mui/icons-material/ExitToApp';
